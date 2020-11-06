c5030f73822a5ee6e201f37e3fb611069bfcbf08
sudachi0114 <jnevariste@gmail.com>
2020-11-07 00:49:15 +0900

    イベント処理のメソッドを定義

--- src/App.js
+++ src/App.js
@@ -5,17 +5,70 @@ import React, { Component } from 'react';
 
 import ChatBox from "./components/ChatBox.js"
 
+let msgRepo = [ {"user_name": "anonymous", "text": "hogera"} ];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onTextChange = this.onTextChange.bind(this)
+    this.onButtonClick = this.onButtonClick.bind(this)
+
+    this.state = {
+      text: "",
+      user_name: "",
+      messages: [],
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div className="App">
         <div className="App-header">
           <h2>Sudachi React Chat App</h2>
         </div>
-        <p> hello react app</p>
-        <ChatBox />
+        <ChatBox onTextChange={this.onTextChange} onButtonClick={this.onButtonClick} />
       </div>
     );
+  }
+
+  // 文字が入力される度に、state の更新を行う method (event 処理) の定義
+  onTextChange(e) {
+    if (e.target.name == 'user_name') {
+      this.setState({
+        "user_name": e.target.value,
+      });
+    } else if (e.target.name == "text") {
+      this.setState({
+        "text": e.target.value,
+      });
+    }
+  }
+
+  // 送信ボタン押下時の event 処理
+  onButtonClick() {
+    // 簡易的バリデーション
+    if (this.state.user_name == '') {
+      alert('user name is empty!')
+      return
+    } else if (this.state.text == '') {
+      alert('text is empty!')
+      return
+    } 
+
+    console.log('yeah!')
+    // this.state.messages.push({
+    //   "user_name": this.state.user_name,
+    //   "text": this.state.text,
+    // })
+    // console.log(this.state.messages)
+
+    msgRepo.push({
+      "user_name": this.state.user_name,
+      "text": this.state.text,
+    })
+    console.log(msgRepo);
+
   }
 }
 
