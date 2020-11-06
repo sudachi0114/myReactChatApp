57ef5a16c932803663446c2d3112984c655c54c6
sudachi0114 <jnevariste@gmail.com>
2020-11-06 20:47:17 +0900

    create chatbox component

---
+++ components/ChatBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ChatBox extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>will be ChatBox</p>
+            </div>
+        );
+    }
+}
